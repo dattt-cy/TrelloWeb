826120b91049c9105a77769303fd96ea5430d894
dattt-cy <victomblack1602@gmail.com>
2025-09-07 15:14:41 +0700

    'refactor: Restructure Board page components'

---
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -0,0 +1,141 @@
+import { useState } from 'react'
+import Box from '@mui/material/Box'
+import Tooltip from '@mui/material/Tooltip'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Divider from '@mui/material/Divider'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Typography from '@mui/material/Typography'
+import ContentCut from '@mui/icons-material/ContentCut'
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
+import Cloud from '@mui/icons-material/Cloud'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { ContentCopy, ContentPaste } from '@mui/icons-material'
+import { AddCard } from '@mui/icons-material'
+import { Button } from '@mui/material'
+import DragHandleIcon from '@mui/icons-material/DragHandle'
+import ListCards from './ListCards/ListCards'
+
+function Column() {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box
+      sx={{
+        minWidth: '300px',
+        maxWidth: '300px',
+        bgcolor: (theme) =>
+          theme.palette.mode === 'dark' ? '#333643' : '#ebecf0',
+        ml: 2,
+        borderRadius: '6px',
+        height: 'fit-content',
+        maxHeight: (theme) =>
+          `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`
+      }}
+    >
+      {/* Box Column Header */}
+      <Box
+        sx={{
+          height: (theme) => theme.trello.columnHeaderHeight,
+          p: 2,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between'
+        }}
+      >
+        <Typography
+          variant='h6'
+          sx={{ fontSize: '1rem', fontWeight: 'bold', cursor: 'pointer' }}
+        >
+          Column Title
+        </Typography>
+        <Box>
+          <Tooltip title='More options'>
+            <ExpandMoreIcon
+              id='basic-column-dropdown'
+              aria-controls={open ? 'basic-menu-column-dropdown' : undefined}
+              aria-haspopup='true'
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+              sx={{ color: 'text.primary', cursor: 'pointer' }}
+            />
+          </Tooltip>
+          <Menu
+            id='basic-menu'
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-column-dropdown'
+            }}
+          >
+            <MenuItem>
+              <ListItemIcon>
+                <AddCard fontSize='small' />
+              </ListItemIcon>
+              <ListItemText>Add new card</ListItemText>
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>
+                <ContentCut fontSize='small' />
+              </ListItemIcon>
+              <ListItemText>Cut</ListItemText>
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>
+                <ContentCopy fontSize='small' />
+              </ListItemIcon>
+              <ListItemText>Copy</ListItemText>
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>
+                <ContentPaste fontSize='small' />
+              </ListItemIcon>
+              <ListItemText>Paste</ListItemText>
+            </MenuItem>
+            <Divider />
+            <MenuItem>
+              <ListItemIcon>
+                <DeleteForeverIcon fontSize='small' />
+              </ListItemIcon>
+              <ListItemText>Remove this column</ListItemText>
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>
+                <Cloud fontSize='small' />
+              </ListItemIcon>
+              <ListItemText>Archive this column</ListItemText>
+            </MenuItem>
+          </Menu>
+        </Box>
+      </Box>
+
+      <ListCards />
+
+      {/* Box Column Footer */}
+      <Box
+        sx={{
+          height: (theme) => theme.trello.columnFooterHeight,
+          p: 2,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between'
+        }}
+      >
+        <Button startIcon={<AddCard />}>Add new card</Button>
+        <Tooltip tile='Drag to move'>
+          <DragHandleIcon sx={{ cursor: 'pointer' }} />
+        </Tooltip>
+      </Box>
+    </Box>
+  )
+}
+
+export default Column
---
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -0,0 +1,36 @@
+import Box from '@mui/material/Box'
+import Card from './Card/Card'
+
+function ListCards() {
+  return (
+    <Box
+      sx={{
+        p: '0 5px',
+        m: '0 5px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 1,
+        overflowX: 'hidden',
+        overflowY: 'auto',
+        maxHeight: (theme) =>
+          `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)} - ${
+            theme.trello.columnHeaderHeight
+          } - ${theme.trello.columnHeaderHeight})`,
+        '&::-webkit-scrollbar-thumb': {
+          backgroundColor: '#ced0da'
+        },
+        '&::-webkit-scrollbar-thumb:hover': {
+          backgroundColor: '#dfc2cf'
+        }
+      }}
+    >
+      <Card />
+      <Card temporaryHideMedia={true} />
+      <Card temporaryHideMedia={true} />
+      <Card temporaryHideMedia={true} />
+      <Card temporaryHideMedia={true} />
+    </Box>
+  )
+}
+
+export default ListCards
